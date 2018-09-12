ef08a26e6d6a0235c59ddbe9608ef69dfb0d15d0
Hopson97 <mhopson@hotmail.co.uk>
2018-09-12 19:36:20 +0100

    document the rpg bot class

--- src/rpg-bot.js
+++ src/rpg-bot.js
@@ -2,12 +2,22 @@ const Discord = require('discord.js');
 const MessageEventHandler = require('./message-event-handler');
 
 
+/**
+ * The main event handler for the bot
+ */
 module.exports = class RPGBot {
+    /**
+     * Construct the bot
+     * @param {Discord.client} client The Discord client
+     */
     constructor(client) {
         this.client  = client;
         this.messageEventHandler = new MessageEventHandler();
     }
 
+    /**
+     * Invoke the command handler
+     */
     runBot() {
         this.client.on("ready", () => {
             console.log("RPG Bot has logged on.");
