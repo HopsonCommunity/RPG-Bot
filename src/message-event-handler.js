37f8c5c597ce866800db9feb8611f577ad4ab1c0
Hopson97 <mhopson@hotmail.co.uk>
2018-09-12 19:23:47 +0100

    Add documentation for the message event handler

--- src/message-event-handler.js
+++ src/message-event-handler.js
@@ -2,13 +2,22 @@ const Discord = require('discord.js');
 const MessageInfo = require('./message-info')
 const RPGCommandHandler = require('./rpg/rpg-commands');
 
+/**
+ * Class to handle messages sent by the user
+ */
 module.exports = class MessageEventHandler {
+    /**
+     * Creates the command handlers and constructs the handler
+     */
     constructor () {
         this.commandHandlers = [
             new RPGCommandHandler()
         ]
     }
-
+    /**
+     * Entry point for handling messages
+     * @param {Discord.TextMessage} message The raw message sent by a user
+     */
     handle (message) {
         const msgInfo = new MessageInfo(message);
         if (msgInfo.isCommand) {
@@ -16,6 +25,10 @@ module.exports = class MessageEventHandler {
         }
     }
 
+    /**
+     * Handles a command message
+     * @param {MessageInfo} msgInfo Info about message sent by user
+     */
     handleCommand(msgInfo) {
         for (let handler of this.commandHandlers) {
             if (handler.isCommand(msgInfo.commandType)) {
